36197ba9b2f2fd2c5a95e80de829c3f77b43f168
Stefan Schneider <stefan.schneider@squareweave.com.au>
2021-09-08 17:11:47 +1000

    Correct pypi category tag

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Framework :: Django :: 2',
+        'Framework :: Django :: 2.0',
         'Topic :: Internet :: WWW/HTTP :: Site Management',
     ],
 
